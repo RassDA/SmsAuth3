c23ba034d26effc09e8742ce29abceef71443b1d
RassDA <infonumru@gmail.com>
2015-02-17 12:47:30 +0300

    - Все сообщения выведены в R.String.  - В поле телефона выводится текст
     - для идентификации используется текст = адрес + случайное число, который передается в тексте смс самому себе.
     - текст сохраняется в настройках приложения.
     - широковещательный приемник перехватывает сообщение и сравнивает текст смс с сохраненным в  настройках приложения.
     - если тексты совпадают, делается вывод о завершенной аутентификации.
     - по результатам выводятся всплывающие подсказки.

--- java/ru/infonum/smsauth/MainActivity.java
+++ java/ru/infonum/smsauth/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.infonum.smsauth;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsManager;
@@ -10,19 +11,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Timestamp;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = "SmsAuth";
 
-    Button sendSMSBtn;
+    Button btnSendSms;
+    Button btnBI;
     EditText editTextTel;
     EditText editTextTxt;
-    final String TESTTEL = "+79119148047";
+
     public static Timestamp sendingTime;
     public static String sendingTimeS;
     public static long sendingTimeL;
@@ -40,29 +44,51 @@ public class MainActivity extends ActionBarActivity {
         //msgTxt = this.getString(R.string.AUTH_STR) + " Отправьте эту смс на свой номер";
         //startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts(msgTxt, msgTel, null)));
 
-        sendSMSBtn = (Button) findViewById(R.id.btnSendSMS);
+        btnSendSms = (Button) findViewById(R.id.btnSendSms);
+        btnBI = (Button) findViewById(R.id.btnBI);
 
         editTextTel = (EditText) findViewById(R.id.editTextPhoneNo);
-        editTextTel.setText(getString(R.string.TESTTEL) + "Введите сюда свой номер");
+        //editTextTel.setText("Введите сюда свой номер");
+        editTextTel.setText(getString(R.string.TESTTEL));
 
         editTextTxt = (EditText) findViewById(R.id.editTextSMS);
-        //final Random random = new Random();
-        editTextTxt.setText(getString(R.string.AUTH_STR));
+        final Random random = new Random();
+        final String randS = String.valueOf(Math.abs(random.nextInt(9999))); // лидирующие нули отбрасываются !!
+        msgTxt = getString(R.string.AUTH_STR_RAND) + randS;
+        editTextTxt.setText(msgTxt);
+
+        final TextView textViewLog = (TextView) findViewById(R.id.textView3);
+
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.clear();
+        editor.putString(getString(R.string.AUTH_RAND), msgTxt);
+        editor.apply();
+
+        Log.d(TAG, "015-- Time=" + new java.sql.Timestamp(System.currentTimeMillis()).toString());
 
         //startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts(msgTxt, msgTel, null)));
         //smsMessageET.setText(String.valueOf(Math.abs(random.nextInt(9999))));
 
-        sendSMSBtn.setOnClickListener(new View.OnClickListener() {
+        btnSendSms.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if (editTextTel.getText().toString().equals("+")) {
-                    sendSmsBI();
-                } else {
-                    sendSMS();
-                }
+                sendSMS();
+                textViewLog.setText("001-- Time=" + new java.sql.Timestamp(System.currentTimeMillis()).toString()
+                        + ": Попытка отправить смс через это приложение\n " + msgTxt );
+                editTextTxt.setText(msgTxt);
+            }
+        });
+
+        btnBI.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSmsBI();
+                textViewLog.setText("001-- Time=" + new java.sql.Timestamp(System.currentTimeMillis()).toString()
+                        + ": Попытка отправлить смс через стандартное смс-приложение\n " + msgTxt);
+
             }
         });
 
     }
+
 
     protected void sendSMS() {
         msgTel = editTextTel.getText().toString();
@@ -75,6 +101,7 @@ public class MainActivity extends ActionBarActivity {
             sendingTime = new java.sql.Timestamp(System.currentTimeMillis());
             sendingTimeL = sendingTime.getTime();
             Log.d(TAG, "001-- Time=" + new java.sql.Timestamp(System.currentTimeMillis()).toString());
+
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "SmsAuth: Сбой в отправке смс. Tel= " + msgTel + "Txt=" + msgTxt,
                     Toast.LENGTH_LONG).show();
@@ -84,12 +111,15 @@ public class MainActivity extends ActionBarActivity {
 
     protected void sendSmsBI() { //Send Sms by Built-in sms manager
         msgTel = editTextTel.getText().toString();
-        msgTxt = editTextTxt.getText().toString() + " Отправьте эту смс на свой номер";
+        //msgTxt = editTextTxt.getText().toString() + " Отправьте эту смс на свой номер";
         Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-        sendIntent.putExtra("sms_body", getString(R.string.AUTH_STR));
+        sendIntent.putExtra("sms_body", msgTxt);
         sendIntent.setType("vnd.android-dir/mms-sms");
         startActivity(sendIntent);
 /*
+
+
+
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts(msgTxt, msgTel, null)));
 
--- java/ru/infonum/smsauth/SMSNotification.java
+++ java/ru/infonum/smsauth/SMSNotification.java
@@ -3,6 +3,7 @@ package ru.infonum.smsauth;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -15,7 +16,7 @@ import java.sql.Timestamp;
  */
 
 public class SMSNotification extends BroadcastReceiver {
-    private static final String AUTH_STR = "infonum.ru/smsauth";
+    //private static final String AUTH_STR = "infonum.ru/smsauth";
     //private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
     public static String msgTel = "";
     public static String msgTxt = "";
@@ -29,15 +30,15 @@ public class SMSNotification extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
         Toast.makeText(context, "SmsAuth перехватило смс", Toast.LENGTH_LONG).show();
-
+        SharedPreferences mPrefs = context.getSharedPreferences(context.getString(R.string.AUTH_RAND), Context.MODE_PRIVATE);
         Bundle bundle = intent.getExtras();
         SmsMessage[] msgs = null;
         String str = "";
         if(bundle != null) {
             Object[] pdus = (Object[]) bundle.get("pdus");
             msgs = new SmsMessage[pdus.length];
+            String rS = mPrefs.getString(context.getString(R.string.AUTH_RAND), "");
 
             for (int i = 0; i < msgs.length; i++) {
                 msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
@@ -50,16 +51,25 @@ public class SMSNotification extends BroadcastReceiver {
 
                 //if (msgTxt.toLowerCase().startsWith(context.getString(R.string.AUTH_STR))
                 //        && ((timeNowL - msgTimeL) > 0)) {
-                if (msgTxt.toLowerCase().startsWith(context.getString(R.string.AUTH_STR))) {
+                //if (msgTxt.toLowerCase().trim().startsWith(rS.toLowerCase().trim())) {
+                String sample = "";
+                //for ( int l=0; l<rS.length() && l<msgTxt.length(); l++){
+                //    sample += rS.charAt(l) + msgTxt.charAt(l);
+                //}
+                if (msgTxt.toLowerCase().contains(rS.toLowerCase().trim())) {
                     Toast.makeText(context, "AUTH OK!", Toast.LENGTH_LONG).show();
-                    Log.d(TAG, "003--" + msgTxt.toLowerCase() + " : " + context.getString(R.string.AUTH_STR));
+                    Log.d(TAG, "003--" + msgTxt.toLowerCase() + " : " + context.getString(R.string.AUTH_STR_RAND) + rS);
 
+                    Intent intent2 = new Intent(this, MainActivity.class )
+                    break;
                 }else{
-                    Toast.makeText(context, "AUTH not OK!!!", Toast.LENGTH_LONG).show();
-                    Log.d(TAG, "004--" + msgTxt.toLowerCase() + " : " + context.getString(R.string.AUTH_STR));
+                    //Toast.makeText(context, "auth NOT ok!!! m1=" + msgTxt.length() + " r2=" + rS.length(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(context, "auth NOT ok!!! m1=" + msgTxt.length() + " r2=" + rS.length(), Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "004--" + msgTxt.toLowerCase() + " : " + context.getString(R.string.AUTH_STR_RAND) + rS);
                 }
+
             }
-            str = "Tel=" + msgTel + ", Text=" + msgTxt + " msgTimeStamp=" + Long.toString(timeNowL - msgTimeL);
+            str = "Tel=" + msgTel + ", Text=" + msgTxt + " Time=" + Long.toString(timeNowL - msgTimeL);
             Log.d(TAG, "002--" + str);
             Toast.makeText(context, str, Toast.LENGTH_LONG).show();
 
